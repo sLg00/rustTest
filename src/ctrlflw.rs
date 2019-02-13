4bc938c2cbafcb48aeb259e8d81ffe666f33b22a
sLg <sulg00@gmail.com>
2019-02-13 21:54:49 +0200

    initial commit

---
+++ src/ctrlflw.rs
@@ -0,0 +1,64 @@
+fn ifstatement(){
+
+    let temp = 15;
+
+    if temp > 30{
+        println!("hawt");
+    }
+
+    else if temp < 10{
+        print!("coldlol");
+    }
+
+    else {
+        println!("ok");
+    }
+
+    let day = if temp > 20{"sunny!"} else{"cloudy"};
+    //if statement can be expressed in the print macro as well
+    println!("today is {}", day);
+}
+
+fn while_loop (){
+
+    let mut x = 1;
+
+    while x < 1000{
+        x = x + 100;
+
+        if x == 801 {continue; }
+        println!("the value is {}", x)
+    }
+
+    let mut y = 1;
+    loop {
+
+        y = y + 100;
+        println!("the valuea is {}", y);
+
+        if y == 901 {break; }
+    }
+}
+
+fn for_loop(){
+
+    for x in 1..11{
+        if x == 8 {break; }
+        println!("x is equal to {}", x);
+    }
+
+    for (pos, y) in (30..41).enumerate(){
+        println!("{} : {}", pos, y);
+    }
+}
+
+fn match_statement(){
+
+}
+
+pub fn main(){
+    //ifstatement();
+    //while_loop();
+    //for_loop();
+    match_statement();
+}
